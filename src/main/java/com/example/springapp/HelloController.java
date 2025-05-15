22328177e4a28985c9baca6d9037aee99b9c88c4
Joke_jonathan <158379000+joke070@users.noreply.github.com>
2025-05-15 12:40:56 +0100

    Trigger workflow rerun

--- src/main/java/com/example/springapp/HelloController.java
+++ src/main/java/com/example/springapp/HelloController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
+ HEAD
         return "Hello chijioke, welcome Spring!";
+
+        return "Hello guys, welcome to my Spring app!";
+ 3161edd (Trigger workflow rerun)
     }
 }
