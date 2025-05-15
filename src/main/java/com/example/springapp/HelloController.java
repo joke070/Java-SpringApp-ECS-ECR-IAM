c010c7826ad0371eeb6679cb0407cb69c9fabc36
Joke_jonathan <158379000+joke070@users.noreply.github.com>
2025-05-15 15:54:38 +0100

    latest code

--- src/main/java/com/example/springapp/HelloController.java
+++ src/main/java/com/example/springapp/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello chijioke, welcome Spring!";
+        return "Hello chijioke, welcome to Spring app!";
     }
 }
